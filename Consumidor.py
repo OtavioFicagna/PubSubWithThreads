7c2e08a3cf0038217f8e92028415014499be7488
Henrique Poerschke <henriquepoerschke@gmail.com>
2024-10-26 18:55:05 -0300

    Avanços no Consumidor

--- Consumidor.py
+++ Consumidor.py
@@ -1,5 +1,6 @@
 import _thread
 import socket
+import pickle
 
 SERVER = '127.0.0.1'
 PORT = 8080
@@ -14,8 +15,20 @@ def inicializar():
     global inscricoes
     inscricoes = list(map(int, input("Insira os tópicos de inscrição: ").split()))
 
+    inscricoes_msg = pickle.dumps(inscricoes)
+
+    tcp.sendto(inscricoes_msg, destino)
+
 def esperar_usuario():
-    raise NotImplementedError()
+    while True:
+        op = input("Operação")
+
+        if op.lower() not in ['sub', 'unsub', 'kill']:
+            print('OPERAÇÃO INVÁLIDA')
+        
+        else:
+            raise NotImplementedError()
+
 
 def exibir_informacoes():
     raise NotImplementedError()
